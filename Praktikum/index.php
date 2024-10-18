16281c7111075d856f39409cfbab80f5a25705de
tuboynibos <ahmadqurthubi28@gmail.com>
2024-10-18 14:10:59 +0700

    tugas 3

---
+++ Praktikum/index.php
@@ -0,0 +1,34 @@
+<?php 
+// Variable
+/**
+ * tidak boleh dimulai angka
+ * tidak boleh mengandung spasi
+ * tidak boleh karakter
+ */
+
+ $a= 10;
+
+//  tipe data
+/**
+ * String
+ * Integer
+ * Float
+ * Boolean
+ */
+
+$name = "L";
+$nilai = 90;
+$phi = 3.14;
+$true = True;
+$false = False;
+
+// Percabangan
+
+
+// forloop
+echo ("<br>");
+for ( $i = 0; $i <10; $i++) {
+    echo $i;
+    echo ("<br>");
+    
+}
---
+++ Praktikum/oop.php
@@ -0,0 +1,16 @@
+<?php
+// Class
+class fruit{
+    public $buah;
+    // method - funngsi
+    // setter
+    public function tampilkanSemuaBuah() {
+        foreach($this-> $buah as $fruit) {
+            echo($fruit);
+        }
+    }
+
+}
+$fruits = new Fruit ();
+$buahBuahan = 
+$fruit1->name = "Duren";
